0e4ab30452151f96dba9ef44f111275f399af829
David <dbstern@gwmail.gwu.edu>
2019-05-08 11:56:07 -0400

    added MAF to consider fixed in NCD prep

--- baypass2NCD_fixed_groups.py
+++ baypass2NCD_fixed_groups.py
@@ -13,6 +13,8 @@ parser.add_argument('-gfile', dest = 'gfile', type = str, required=True,  help =
 parser.add_argument('-snpdet', dest = 'snpdet', type = str, required=True,  help = 'input snpdet file')
 parser.add_argument('-g1', dest = 'g1', type = int, required=True,  help = 'number of pops in group 1 (assuming from the start)')
 parser.add_argument('-g2', dest = 'g2', type = int, required=True,  help = 'number of pops in group 2 (assuming the last ones)')
+parser.add_argument('-m', dest = 'maf', type = float, required=True,  help = 'minimum minor allele frequency to consider fixed')
+
 
 ### NCD Format: CHR	POS	REF	Chimp_REF	ID
 
@@ -27,16 +29,16 @@ for line_gfile, line_snpdet in izip(open(args.gfile), open(args.snpdet)):
 	scafnum = str(scaf.strip("Scaffold"))
 	ID = scafnum+'|'+pos
 	ref_allele = snpinfo[3]
-	alt_allele = snpinfo[4]
+	alt_allele = snpinfo[4].strip('\n')
 	ref_g1 = map(int, [ dat[i] for i in range(0,args.g1*2,2)])
 	alt_g1 = map(int, [ dat[i] for i in range(1,args.g1*2,2)])
 	ref_g2 = map(int,[ dat[i] for i in range((args.g1*2),(args.g1+args.g2)*2,2)])
 	alt_g2 = map(int,[ dat[i] for i in range((args.g1*2)+1,(args.g1+args.g2)*2,2)])
 	g1_AF = sum(ref_g1) / (sum(ref_g1)+sum(alt_g1))
 	g2_AF = sum(ref_g2) / (sum(ref_g2)+sum(alt_g2))
-	if g1_AF == 0 and g2_AF == 1:
+	if g1_AF < args.maf and g2_AF > (1-args.maf):
 		output.write(scafnum+'\t'+ pos +'\t'+ref_allele+'\t'+alt_allele+'\t'+ID+'\n')
-	elif g1_AF == 1 and g2_AF == 2:
+	elif g1_AF > (1-args.maf) and g2_AF < args.maf:
 		output.write(scafnum+'\t'+ pos +'\t'+ref_allele+'\t'+alt_allele+'\t'+ID+'\n')
 	else:
 		pass
