1a586fb7850e566a41a619882b802cd1de7d963d
David <dbstern@gwmail.gwu.edu>
2019-07-10 20:21:49 -0500

    made coverage calculator run with parallel processing

--- calculate_coverage_distribution_sync.py
+++ calculate_coverage_distribution_sync.py
@@ -4,39 +4,34 @@ import argparse
 import numpy as np
 import time
 import progressbar
+from joblib import Parallel, delayed
+import multiprocessing
 
 parser = argparse.ArgumentParser(description='Script to calculate the top X percentage of coverage across all pools')
 parser.add_argument('-i', dest = 'input', type = str, required=True,  help = 'input sync file')
 parser.add_argument('-q', dest = 'quantile', type = float, required=True,  help = 'coverage percentile to calculate (e.g. 0.99 will calculate coverage at top 99%% of sites')
+parser.add_argument('-o', dest = 'output', type = str, required=True,  help = 'output file')
+
 
 args = parser.parse_args()
 
-def file_len(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
-print("Calculating file length")
-flen = file_len(args.input)
-print("Done")
+output = open(args.output, 'w')
+    
+def calc_cov_per_line(line):
+	popdat = line.split('\t')[3:]
+	for pop in popdat:
+		cov = sum(map(int,pop.split(':')))
+	return(cov)
 
 print("Calculating maximum coverage cutoffs from the empirical distributions of coverages")
-
+num_cores = multiprocessing.cpu_count()
 cov_dat = []
-pbar = progressbar.ProgressBar(maxval=int(flen))
-pbar.start()
 
 with open(args.input,'rU') as f:
-	i=1
-	for line in f:
-		popdat = line.split('\t')[3:]
-		for pop in popdat:
-			cov = sum(map(int,pop.split(':')))
-			cov_dat.append(cov)
-		time.sleep(0.05)
-		pbar.update(i+1)
-pbar.finish()
+	cov = Parallel(n_jobs=num_cores)(delayed(calc_cov_per_line)(line) for line in f)
+	cov_dat.append(cov)
 
 cov_dat = np.array(cov_dat)
 top_cov = int(np.quantile(cov_dat, args.quantile,interpolation="nearest"))
-print("Result: 'max-coverage %s' is equivalent to 'max-coverage %s'\n"%(args.quantile,top_cov))
+print("Done")
+output.write("Result: 'max-coverage %s' is equivalent to 'max-coverage %s'\n"%(args.quantile,top_cov))
